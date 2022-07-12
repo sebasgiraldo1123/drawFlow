71c3c1fa49f74808c45d04b77143f9396939fac8
sebasgiraldo1123 <sebasgiraldo1123@gmail.com>
2022-07-11 21:11:45 -0500

    Conexion dgo v1.0

--- Backend/Client.go
+++ Backend/Client.go
@@ -1,9 +1,27 @@
 package main
 
-import(
+import (
 	"fmt"
+	dgo "github.com/dgraph-io/dgo/v200"
+	"github.com/dgraph-io/dgo/v200/protos/api"
+	"google.golang.org/grpc"
+	"log"
 )
 
-func main(){
+func main() {
 	fmt.Println("Hello Backend")
+	client := newClient()
+	fmt.Println(client)
+
+}
+
+func newClient() *dgo.Dgraph {
+	// Dial a gRPC connection. The address to dial to can be configured when
+	// setting up the dgraph cluster.
+	conn, err := grpc.Dial("localhost:9080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return dgo.NewDgraphClient(api.NewDgraphClient(conn))
 }

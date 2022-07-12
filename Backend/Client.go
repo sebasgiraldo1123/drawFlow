7172e10b8b3eb0cd39050eebae8770dabe4b2dac
sebasgiraldo1123 <sebasgiraldo1123@gmail.com>
2022-07-12 08:58:24 -0500

    Actualización gitignore

--- Backend/Client.go
+++ Backend/Client.go
@@ -1,27 +1,16 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
+
 	dgo "github.com/dgraph-io/dgo/v200"
 	"github.com/dgraph-io/dgo/v200/protos/api"
 	"google.golang.org/grpc"
-	"log"
 )
 
 func main() {
-	fmt.Println("Hello Backend")
-	client := newClient()
-	fmt.Println(client)
 
-}
-
-func newClient() *dgo.Dgraph {
-	// Dial a gRPC connection. The address to dial to can be configured when
-	// setting up the dgraph cluster.
-	conn, err := grpc.Dial("localhost:9080", grpc.WithInsecure())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return dgo.NewDgraphClient(api.NewDgraphClient(conn))
 }
---
+++ Backend/go.mod
@@ -0,0 +1,19 @@
+module modulos
+
+go 1.18
+
+require (
+	github.com/dgraph-io/dgo/v200 v200.0.0-20210401091508-95bfd74de60e
+	google.golang.org/grpc v1.27.0
+)
+
+require (
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.4.1 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/sys v0.0.0-20200930185726-fdedc70b468f // indirect
+	golang.org/x/text v0.3.3 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
